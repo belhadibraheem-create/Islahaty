953bf2e5d5d563a6cceab3803e20076f553c8b66
belhadibraheem-create <bel.hadibraheem@gmail.com>
2026-01-01 19:13:10 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+{
+  "name": "Islahaty",
+  "version": "1.0.0",
+  "description": "A Moroccan platform connecting customers with craftsmen for home repairs.",
+  "main": "index.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1"
+  },
+  "devDependencies": {},
+  "author": "Your Name",
+  "license": "MIT"
+}
