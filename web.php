1fb049ec9a05c26dc3f0bd287083432b83c02f59
belhadibraheem-create <bel.hadibraheem@gmail.com>
2026-01-01 19:14:12 +0100

    Add files via upload

---
+++ web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::post('/request-service', [ServiceController::class, 'store']);
